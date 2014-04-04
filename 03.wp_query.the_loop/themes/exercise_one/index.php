8c3924e0d05ca0b574e92f5a8e32e6a79791fc3d
pkostadinov-2create <pkostadinov.2create@gmail.com>
2014-04-04 14:55:13 +0300

    WP Query exercise 1 Done

--- 03.wp_query.the_loop/themes/exercise_one/index.php
+++ 03.wp_query.the_loop/themes/exercise_one/index.php
@@ -3,26 +3,27 @@
 <body <?php body_class($class); ?>>
 	<div class="posts-container">
 		<h2>Най-новите 5 поста</h2>
-		<?php $latest_five_posts = array('posts_per_page' => 5); ?>
-		<?php exercise_one_print_posts($latest_five_posts); ?>
+		<?php exercise_one_print_posts(array('posts_per_page' => 5)); ?>
 	</div><!-- /.posts-container -->
 
 	<div class="posts-container">
 		<h2>Най-старите 3 поста</h2>
-		<?php $oldest_three_posts = array(
+		<?php 
+		exercise_one_print_posts(array(
 			'posts_per_page' => 3,
 			'order' => 'ASC'
-		); ?>
-		<?php exercise_one_print_posts($oldest_three_posts); ?>
+		)); 
+		?>
 	</div><!-- /.posts-container -->
 
 	<div class="posts-container">
 		<h2>Най-новите 2 поста от категорията News</h2>
-		<?php $latest_two_news_posts = array(
+		<?php
+		exercise_one_print_posts(array(
 			'posts_per_page' => 2,
 			'category_name' => 'news'
-		); ?>
-		<?php exercise_one_print_posts($latest_two_news_posts); ?>
+		));
+		?>
 	</div><!-- /.posts-container -->
 
 	<div class="posts-container">
@@ -33,44 +34,42 @@
 			get_term_by('slug', 'blog', 'category')->term_id
 		);
 		
-		$oldest_blog_and_news_posts = array(
+		exercise_one_print_posts(array(
 			'posts_per_page' => '1',
 			'order' => 'ASC',
 			'category_and' => $exercise_one_category_ids
-		); ?>
-		<?php exercise_one_print_posts($oldest_blog_and_news_posts); ?>
+		));
+		?>
 	</div><!-- /.posts-container -->
 
 	<div class="posts-container">
 		<h2>Всички постове, които са едновременно в категория News и в таг website, подредени по заглавие в увеличаващ се ред</h2>
-		<?php $cat_news_and_tag_website_posts_sort_by_title_asc = array(
+		<?php
+		exercise_one_print_posts(array(
 			'posts_per_page' => '-1',
 			'orderby' => 'title',
 			'order' => 'ASC',
 			'category_name' => 'news',
 			'tag' => 'website'
-		); ?>
-		<?php exercise_one_print_posts($cat_news_and_tag_website_posts_sort_by_title_asc); ?>
+		));
+		?>
 	</div><!-- /.posts-container -->
 
 	<div class="posts-container">
 		<h2>Най-стария пост, който е едновременно в категория Events и в таг conference, от декември, 2012.</h2>
-		<?php $oldest_one_cat_events_tag_conference_after_dec_twenty_twelve_posts = array(
+		<?php
+		exercise_one_print_posts(array(
 			'date_query' => array(
-				array(
-					'month'      => 12,
-					'compare'   => '>=',
+					array(
+						'after' => 'December 2012',
+					),
 				),
-				array(
-					'year'      => 2012,
-					'compare'   => '>=',
-				)
-			),
 			'posts_per_page' => 1,
 			'order' => 'ASC',
 			'category_name' => 'events',
-			'tag' => 'conference'); ?>
-		<?php exercise_one_print_posts($oldest_one_cat_events_tag_conference_after_dec_twenty_twelve_posts); ?>
+			'tag' => 'conference'
+		));
+		?>
 	</div><!-- /.posts-container -->
 
 	<div class="posts-container">
@@ -79,14 +78,14 @@
 		// $exercise_one_blog_id = get_term_by('slug', 'blog', 'category');
 		// $exercise_one_blog_id = array($exercise_one_blog_id->term_id);
 
-		// $cat_news_notcat_blog_posts_sort_random = array(
+		// exercise_one_print_posts(array(
 		// 	'posts_per_page' => -1,
 		// 	'category_name' => 'news',
 		// 	'category__not_in' => $exercise_one_blog_id,
 		// 	'orderby' => 'rand',
-		// );
+		// ));
 
-		$cat_news_notcat_blog_posts_sort_random = array(
+		exercise_one_print_posts(array(
 			'posts_per_page' => -1,
 			'tax_query' => array (
 				'relation' => 'AND',
@@ -103,9 +102,8 @@
 				),
 			),
 			'orderby' => 'rand',
-		);
-		 ?>
-		<?php exercise_one_print_posts($cat_news_notcat_blog_posts_sort_random); ?>
+		));
+		?>
 	</div><!-- /.posts-container -->
 </body>
 
