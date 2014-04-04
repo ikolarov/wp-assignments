09f40f937269639bb742f587372c9e7cba3bc478
pkostadinov-2create <pkostadinov.2create@gmail.com>
2014-04-04 14:55:50 +0300

    WP Query exercise 2 Done

--- 03.wp_query.the_loop/themes/exercise_two/functions.php
+++ 03.wp_query.the_loop/themes/exercise_two/functions.php
@@ -9,42 +9,47 @@ function exercise_one_print_posts( $query_vars ) {
 	if ( $custom_query->have_posts() ) :
 		while ( $custom_query->have_posts() ) :
 			$custom_query->the_post();
-			$additional_post_classes = array(); 
 
-			$additional_post_classes[] = $custom_query->current_post % 2 ? 'odd' : 'even'; // Add Odd Even class
+			$additional_post_classes = array(); 
+			$additional_post_classes[] = $custom_query->current_post % 2 ? 'odd' : 'even';
 
 			if ($custom_query->current_post === 0 ) {
-				$additional_post_classes[] = 'first'; // Add first class on the first post
+				$additional_post_classes[] = 'first';
 			}
 			if ($custom_query->current_post === ($custom_query->post_count - 1) ) {
-				$additional_post_classes[] = 'last'; // Add last class on the last post
+				$additional_post_classes[] = 'last';
 			}
+
+			$attachments = get_children(array(
+				'post_parent' => get_the_ID(), 
+				'post_mime_type' => 'image')
+			);
+			$images_count = count($attachments); 
+
+			$post_content = wp_trim_words( get_the_content(), 20 );
+			$post_content = apply_filters( 'the_content', $post_content );
 			?>
 
 			<div <?php post_class($additional_post_classes); ?>>
 				<h3>
 					<a href="<?php the_permalink(); ?>">
-						<?php if ( has_tag('website') ) { echo 'Web: '; } ?>
-						<?php echo exercise_one_get_the_title(); ?>
-						<?php if ( has_category('News') ) { echo '!'; } ?>
+						<?php echo has_tag('website') ? 'Web: ' : ''; ?>
+						<?php the_title(); ?>
+						<?php echo has_category('News') ? '!' : ''; ?>
 					</a>
 				</h3>
+
 				<small>
 					Posted <?php echo human_time_diff( get_the_time('U') ); ?> ago
 					in Categories: <?php the_category(","); ?>
 					<?php the_tags("with tags: ", ", "); ?>
 					by <?php the_author(); ?>
-					<?php 
-					$attachments = get_children(array('post_parent'=>get_the_ID(), 'post_mime_type' => 'image'));
-					$images_count = count($attachments); ?>
 					[<?php echo $images_count; ?> Images]
 					[<?php echo $custom_query->current_post; ?> Post]
 				</small>
 
 				<div class="post-content">
-					<?php 
-					$mycontent = wp_trim_words( get_the_content(), 20 );
-					echo apply_filters( 'the_content', $mycontent ); ?>
+					<?php echo $post_content; ?>
 				</div><!-- /.post-content -->
 			</div>
 		<?php
@@ -52,11 +57,10 @@ function exercise_one_print_posts( $query_vars ) {
 	endif;
 }
 
-function exercise_one_get_the_title () {
-	$title = get_the_title();
-
+add_filter( 'the_title', 'exercise_one_get_the_title' );
+function exercise_one_get_the_title ($title) {
 	if ( is_search() ) {
-		$title = preg_replace('/' . get_search_query() . '/i', "<span class=\"search-match\">$0</span>", $title);
+		$title = preg_replace('/' . preg_quote(get_search_query(), '/') . '/i', '<span class="search-match">$0</span>', $title);
 	}
 
 	return $title;
--- 03.wp_query.the_loop/themes/exercise_two/index.php
+++ 03.wp_query.the_loop/themes/exercise_two/index.php
@@ -1,6 +1,4 @@
 <?php get_header(); ?>
-
-<body <?php body_class($class); ?>>
 	<div class="posts-container">
 		<h2>Category: Blog, News, Events</h2>
 		<h6>All posts</h6>
@@ -13,18 +11,13 @@
 			get_term_by('slug', 'events', 'category')->term_id,
 		);
 
-
-		$today = getdate();
-		
 		$latest_five_posts = array(
 			'category__in' => $exercise_two_category_ids,
-			'year' => $today["year"],
+			// 'year' => date('Y'),
 			'orderby' => 'title',
 			'order' => 'ASC',
 			'posts_per_page' => -1,
 		); ?>
 		<?php exercise_one_print_posts($latest_five_posts); ?>
 	</div><!-- /.posts-container -->
-</body>
-
 <?php get_footer(); ?>
